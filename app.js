a96badd2524b18324ca0078248cfa167609ae3d2
yuka <fiels.yuka@gmail.com>
2021-06-21 15:12:20 +0900

    created API structure

--- app.js
+++ app.js
@@ -1,17 +1,95 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.get('/', (req, res) => {
-  // res.status(200).send('Hello from the server side.');
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side.', app: 'Natours' });
-});
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
-});
+const getTour = (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No valid id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: '<Updated tour here...>',
+        },
+      });
+    }
+  );
+};
+
+const updateTour = (req, res) => {
+  if (res.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No valid id',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  if (res.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'No valid id',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+app.route('/app/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/app/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
